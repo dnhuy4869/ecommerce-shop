6b114e540c320fcaa0134b7bccf87fec27656687
Huy <73681373+dnhuy4869@users.noreply.github.com>
2023-07-19 01:24:44 +0700

    base layout

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+const App = () => {
+    
+    return (
+        <>
+           <BrowserRouter>
+                <Routes>
+                    <Route path="/" exact element={<Home />} />
+                </Routes>
+           </BrowserRouter>
+        </>
+    )
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import teal from '@mui/material/colors/teal';
+
+const theme = createTheme({
+    palette: {
+        primary: teal,
+        secondary: teal,
+    },
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
+);
